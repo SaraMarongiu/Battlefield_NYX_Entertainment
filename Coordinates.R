a6a59b787ad1bc3e1bf36c4cbc893526b520c9d5
SaraMarongiu <48964715+SaraMarongiu@users.noreply.github.com>
2021-01-28 01:41:21 +0100

    Add files via upload
    First part of the battleship game code. It contains a class coordinates and a class battlefield and a class shots.

---
+++ Coordinates.R
@@ -0,0 +1,98 @@
+# I create the class Coordinates, useful to check if the coordinates of a ship's position are valid (so, both 1<x<10 and 1<y<10)
+
+Coordinates <- setRefClass("Coordinates",
+                           fields=list(
+                             col = "numeric",
+                             row = "numeric", 
+                             isValid = "logical"
+                           ),
+                           methods=list(
+                             initialize = function(newCol, newRow) {
+                               col <<- newCol
+                               row <<- newRow
+                               # We need to check that col and row are valid
+                               # coordinates for the chessboard (so >= 1 and <= 10)
+                               if ((col >= 1) && (col <= 10) && 
+                                   (row >= 1) && (row <= 10)) {
+                                 isValid <<- TRUE
+                               } else {
+                                 isValid <<- FALSE
+                               }
+                             }
+                           ))
+
+# I am creating the class Battlefield.
+
+Battlefield <- setRefClass("Battlefield",
+                           fields=list(
+                             battlefield = "matrix",
+                             player = "numeric"
+                           ),
+                           methods=list(
+                             initialize = function() {
+                               # I used "dimnames" to have all rows named with a letter, as in the battleship game.
+                               battlefield <<- matrix(list(NULL), nrow = 10, ncol = 10, dimnames = list(c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")))
+                             },
+                             
+                             # here I define the look of the grid, putting | and --- to create the "drawing".
+                             
+                             draw = function() {
+                               cat("    ")
+                               for (header in rownames(battlefield)) {
+                                 cat(header, "   ", sep="")
+                               }
+                               cat("\n")
+                               for (row in seq(1, 10)) {
+                                 if (row != 10){
+                                   cat(row, " ", sep="")
+                                 }  else {
+                                   cat(row, "", sep="")
+                                 }
+                                 for (col in seq(1, 10)) {
+                                   cat("|   ")
+                                 }
+                                 cat("|\n  ")
+                                 for (col in seq(1, 10)) {
+                                   cat("----")
+                                 }
+                                 cat("-\n")
+                               }
+                             }
+                           ))
+
+Shots <- setRefClass("Shots",
+                     fields=list(
+                       battlefield = "matrix",
+                       player = "numeric"
+                     ),
+                     methods=list(
+                       initialize = function() {
+                         # I used "dimnames" to have all rows named with a letter, as in the battleship game.
+                         battlefield <<- matrix(list(NULL), nrow = 10, ncol = 10, dimnames = list(c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")))
+                       },
+                       
+                       # here I define the look of the grid, putting | and --- to create the "drawing".
+                       
+                       draw = function() {
+                         cat("    ")
+                         for (header in rownames(battlefield)) {
+                           cat(header, "   ", sep="")
+                         }
+                         cat("\n")
+                         for (row in seq(1, 10)) {
+                           if (row != 10){
+                             cat(row, " ", sep="")
+                           }  else {
+                             cat(row, "", sep="")
+                           }
+                           for (col in seq(1, 10)) {
+                             cat("|   ")
+                           }
+                           cat("|\n  ")
+                           for (col in seq(1, 10)) {
+                             cat("----")
+                           }
+                           cat("-\n")
+                         }
+                       }
+                     ))
